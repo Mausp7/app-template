55a8bdee5db8040c24e3b3a6739eaa24cc45d26a
Mausp7 <archie.tombacz@gmail.com>
2022-06-10 12:39:41 +0200

    Readme update.

--- backend/README.md
+++ backend/README.md
@@ -9,8 +9,23 @@
 
 Create .env file in root and definte the following variables:
 
-- PORT={4000}
-- APP_URL={http://localhost:3000}
+PORT=4000
+APP_URL=http://localhost:3000
+CONNECTION_STRING=mongodb://localhost:27017/templateDB
+
+JWT_SECRET=verySecretToken
+
+//logger
+LOGFLARE_SCOURCE_ID=
+LOGFLARE_API_KEY=
+
+GOOGLE_CLIENT_ID=
+GOOGLE_CLIENT_SECRET=
+GOOGLE_REDIRECT_URI=http://localhost:3000/callback
+
+GITHUB_CLIENT_ID=
+GITHUB_CLIENT_SECRET=
+GITHUB_CLIENT_REDIRECT_URI=http://localhost:3000/redirect
 
 ## dev start:
 
--- backend/app.config.js
+++ backend/app.config.js
@@ -1,9 +1,9 @@
 const config = {
     auth: {
         google: { //https://accounts.google.com/o/oauth2/v2/auth?client_id=423125049963-vnhlm59vvirdjsquu0efhqvq5u91orks.apps.googleusercontent.com&response_type=code&scope=openid&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&prompt=select_account&flowName=GeneralOAuthFlow
-            clientId : process.env.GOOGLE_CLIENT_ID || "423125049963-vnhlm59vvirdjsquu0efhqvq5u91orks.apps.googleusercontent.com",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ||"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",
-            redirectUri: process.env.GOOGLE_REDIRECT_URI ||"http://localhost:3000/callback",
+            clientId : process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+            redirectUri: process.env.GOOGLE_REDIRECT_URI || "http://localhost:3000/callback",
             tokenEndpoint: "https://oauth2.googleapis.com/token",
             grantType: "authorization code",
             userTokenEndpoint: null,
@@ -11,8 +11,8 @@ const config = {
             
         },
         github: { //https://github.com/login/oauth/authorize?client_id=758b2d68f96a29a72f44&response_type=code&scope=user&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fredirect&prompt=select_account&flowName=GeneralOAuthFlow
-            clientId : process.env.GITHUB_CLIENT_ID || "758b2d68f96a29a72f44",
-            clientSecret: process.env.GITHUB_CLIENT_SECRET || "23383634d7829012811d7688d01b64f077c6aad9",
+            clientId : process.env.GITHUB_CLIENT_ID || "",
+            clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
             redirectUri: process.env.GITHUB_CLIENT_REDIRECT_URI || "http://localhost:3000/redirect",
             tokenEndpoint: "https://github.com/login/oauth/access_token",
             grantType: "authorization code",
